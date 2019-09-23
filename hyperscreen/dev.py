4057c11f243e878f0b1f87bd07623e79c60f9c08
Grant Tremblay <granttremblay@gmail.com>
2019-09-23 15:15:03 -0400

    Update dev.py

--- hyperscreen/dev.py
+++ hyperscreen/dev.py
@@ -13,9 +13,3 @@ import hyperscreen
 archive_path = '/Users/grant/Science/HRC_Database/EVT1_Files/'
 evt1_files = glob.glob(archive_path + '**/*evt1*', recursive=True)
 
-for observation in evt1_files:
-    evt1 = hyperscreen.HRCevt1(observation)
-    instrument = evt1.detector
-    size = evt1.numevents
-    obsid = evt1.obsid
-    print('{} is {} with {} events'.format(obsid, instrument, size))
