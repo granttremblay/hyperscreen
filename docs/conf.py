42a2425aaf25b1c744b0195779d864ad50ddce80
Grant Tremblay <granttremblay@gmail.com>
2019-10-07 09:42:09 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -118,10 +118,8 @@ html_theme_options = {
 
 
 
-# import astropy_sphinx_theme
-# html_theme_path = astropy_sphinx_theme.get_html_theme_path()
-
-
+import astropy_sphinx_theme
+html_theme_path = astropy_sphinx_theme.get_html_theme_path()
 
 html_theme = 'bootstrap-astropy'
 
