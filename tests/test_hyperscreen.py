64390a066c317b522a6c39dd18fea8ba1ce9e48c
Grant Tremblay <granttremblay@gmail.com>
2019-10-06 21:46:13 -0400

    fix

--- tests/test_hyperscreen.py
+++ tests/test_hyperscreen.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `hyperscreen` package."""
-
-
+"""
+py.test unit tests for hyperscreen and related packages. 
+Currently, these are pretty dumb. 
+"""
 
 import sys
 import os
-# Set the path explicitly #
-sys.path.insert(0, os.path.abspath(__file__+"/../"))
-from hyperscreen import hyperscreen
 
 import pytest
 
+from hyperscreen import hyperscreen
+
 
 @pytest.fixture()
-def loadHRCI():
-    hrcI = hyperscreen.HRCevt1('data/hrcI_evt1_testfile.fits.gz')
-    return hrcI
+def hrcI_evt1():
+    hrcI_file = os.path.abspath(os.path.dirname(os.path.abspath(__file__))+'/data/hrcI_evt1_testfile.fits.gz')
+    hrcI_evt1 = hyperscreen.HRCevt1(hrcI_file)
+    return hrcI_evt1
 
-def test_structure(loadHRCI):
-    assert hrcI.numevents > 0
+@pytest.fixture()
+def hrcS_evt1():
+    hrcS_file = os.path.abspath(os.path.dirname(os.path.abspath(__file__))+'/data/hrcS_evt1_testfile.fits.gz')
+    hrcS_evt1 = hyperscreen.HRCevt1(hrcS_file)
+    return hrcS_evt1
 
+def test_hrcI_evt1(hrcI_evt1):
+    assert len(hrcI_evt1.data['fp_u']) == len(hrcI_evt1.data['time'])
+
+def test_hrcS_evt1(hrcS_evt1):
+    assert len(hrcS_evt1.data['fp_u']) == len(hrcS_evt1.data['time'])
+
+def test_gti_masks(hrcI_evt1, hrcS_evt1):
+    match_I = hrcI_evt1.badtimeevents == hrcI_evt1.numevents - hrcI_evt1.goodtimeevents
+    match_S = hrcS_evt1.badtimeevents == hrcS_evt1.numevents - hrcS_evt1.goodtimeevents
+    assert match_I
+    assert match_S
+
+# def test_legacy_hyperbola_status_bits(hrcI_evt1, hrcS_evt1)
